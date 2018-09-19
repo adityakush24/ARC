ccf03f6dfee0aa628604d2b7c183823a4e722b8f
adityakush24 <adityakush24@users.noreply.github.com>
2018-09-19 20:07:38 +0530

    Added Ripple Effect

--- app/src/main/java/com/adityakush24/arc/MainActivity.java
+++ app/src/main/java/com/adityakush24/arc/MainActivity.java
@@ -1,13 +1,20 @@
 package com.adityakush24.arc;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+
+import com.skyfishjy.library.RippleBackground;
 
 public class MainActivity extends AppCompatActivity {
+    ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final RippleBackground rippleBackground= findViewById(R.id.content);
+        rippleBackground.startRippleAnimation();
+
     }
 }
