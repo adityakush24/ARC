fcbaa8dee80b4dc7e9806657759fc24e62b9aaa0
ayush-sri <ayushsrivastva23@gmail.com>
2018-10-02 10:33:55 +0530

    new UI design implemented

--- app/src/main/java/com/adityakush24/arc/MainActivity.java
+++ app/src/main/java/com/adityakush24/arc/MainActivity.java
@@ -1,36 +1,47 @@
 package com.adityakush24.arc;
 
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ebanx.swipebtn.OnActiveListener;
-import com.ebanx.swipebtn.OnStateChangeListener;
 import com.ebanx.swipebtn.SwipeButton;
 import com.skyfishjy.library.RippleBackground;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Animation.AnimationListener {
 
     SwipeButton enableButton;
     Button loginButton;
+    TextView register;
     ConstraintLayout constraintLayout;
+    Animation slideup;
+    EditText email,username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        register = (TextView)findViewById(R.id.register);
+        slideup = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.slide_up);
+        slideup.setAnimationListener(this);
         final RippleBackground rippleBackground= findViewById(R.id.content);
         rippleBackground.startRippleAnimation();
-        constraintLayout = findViewById(R.id.login_layout);
+
+        /*constraintLayout = findViewById(R.id.login_layout);
         AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
         animationDrawable.setEnterFadeDuration(2500);
         animationDrawable.setExitFadeDuration(5000);
-        animationDrawable.start();
+        animationDrawable.start();0
         enableButton = findViewById(R.id.swipe_btn);
-        loginButton = findViewById(R.id.login_btn);
+        */
+        loginButton = (Button)findViewById(R.id.loginbtn);
         enableButton.setOnActiveListener(new OnActiveListener() {
             @Override
             public void onActive() {
@@ -38,10 +49,28 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Register", Toast.LENGTH_SHORT).show();
             }
         });
+      register.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              rippleBackground.setVisibility(View.INVISIBLE);
+              loginButton.setVisibility(View.VISIBLE);
+              loginButton.startAnimation(slideup);
+          }
+      });
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
 
     }
-    public void isWorking(View view)
-    {
-        Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show();
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 }
