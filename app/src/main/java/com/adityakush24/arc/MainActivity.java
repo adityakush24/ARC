94fec8bc170d63b790bf76ed8fca8c0a0666c25a
ayush-sri <ayushsrivastva23@gmail.com>
2018-10-20 21:54:16 +0530

    new UI design implemented

--- app/src/main/java/com/adityakush24/arc/MainActivity.java
+++ app/src/main/java/com/adityakush24/arc/MainActivity.java
@@ -3,9 +3,11 @@ package com.adityakush24.arc;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,15 +21,20 @@ public class MainActivity extends AppCompatActivity implements Animation.Animati
 
     SwipeButton enableButton;
     Button loginButton;
-    TextView register;
+    TextView register,forgotPass;
     ConstraintLayout constraintLayout;
     Animation slideup;
-    EditText email,username;
+    EditText email,password,confPass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email = (EditText)findViewById(R.id.email);
+        password = (EditText)findViewById(R.id.password);
         register = (TextView)findViewById(R.id.register);
+        confPass = (EditText)findViewById(R.id.confirm_password);
+        forgotPass = (TextView)findViewById(R.id.forgt_pass);
         slideup = AnimationUtils.loadAnimation(getApplicationContext(),
                 R.anim.slide_up);
         slideup.setAnimationListener(this);
@@ -49,11 +56,36 @@ public class MainActivity extends AppCompatActivity implements Animation.Animati
                 Toast.makeText(MainActivity.this, "Register", Toast.LENGTH_SHORT).show();
             }
         });*/
+
       register.setOnClickListener(new View.OnClickListener() {
           @Override
           public void onClick(View view) {
-              loginButton.setVisibility(View.VISIBLE);
-              loginButton.startAnimation(slideup);
+              if(register.getText().equals("don't have an account ? sign up")) {
+                  Animation move = new TranslateAnimation(Animation.ABSOLUTE, Animation.ABSOLUTE, Animation.ABSOLUTE, -50);
+                  Animation mov = new TranslateAnimation(Animation.ABSOLUTE, Animation.ABSOLUTE, Animation.ABSOLUTE, -55);
+                  mov.setDuration(1000);
+                  mov.setFillAfter(true);
+                  move.setDuration(1000);
+                  move.setFillAfter(true);
+                  email.startAnimation(move);
+                  password.startAnimation(mov);
+                  confPass.setVisibility(View.VISIBLE);
+                  confPass.startAnimation(slideup);
+                  forgotPass.setVisibility(View.INVISIBLE);
+                  loginButton.setText("SIGN UP");
+                  register.setText("Back to login.");
+              }
+              else {
+                  Animation move = new TranslateAnimation(Animation.ABSOLUTE, Animation.ABSOLUTE, Animation.ABSOLUTE, 50);
+                  move.setDuration(500);
+                  move.setFillAfter(true);
+                  email.startAnimation(move);
+                  password.startAnimation(move);
+                  confPass.setVisibility(View.INVISIBLE);
+                  forgotPass.setVisibility(View.VISIBLE);
+                  loginButton.setText("SIGN IN");
+                  register.setText("don't have an account ? sign up");
+              }
           }
       });
     }
